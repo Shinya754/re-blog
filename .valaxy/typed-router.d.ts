9dd2a90be1094f0a58a7d4f4f1be6df5826b9882
shinya754 <578349819@qq.com>
2024-04-15 10:44:16 +0800

    20240415update

--- .valaxy/typed-router.d.ts
+++ .valaxy/typed-router.d.ts
@@ -27,8 +27,7 @@ declare module 'vue-router/auto-routes' {
     '/categories/': RouteRecordInfo<'/categories/', '/categories', Record<never, never>, Record<never, never>>,
     '/links/': RouteRecordInfo<'/links/', '/links', Record<never, never>, Record<never, never>>,
     '/page/[page]': RouteRecordInfo<'/page/[page]', '/page/:page', { page: ParamValue<true> }, { page: ParamValue<false> }>,
-    '/posts/hello-valaxy': RouteRecordInfo<'/posts/hello-valaxy', '/posts/hello-valaxy', Record<never, never>, Record<never, never>>,
-    '/posts/test': RouteRecordInfo<'/posts/test', '/posts/test', Record<never, never>, Record<never, never>>,
+    '/posts/2024-04-15-博客重启': RouteRecordInfo<'/posts/2024-04-15-博客重启', '/posts/2024-04-15-博客重启', Record<never, never>, Record<never, never>>,
     '/tags/': RouteRecordInfo<'/tags/', '/tags', Record<never, never>, Record<never, never>>,
   }
 }
--- pages/posts/test.md
+++
@@ -1,5 +0,0 @@
----
-layout: post
-title: test
-date: 2024-04-09 11:15:11
----
